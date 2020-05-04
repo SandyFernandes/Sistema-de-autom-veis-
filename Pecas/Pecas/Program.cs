b6ee4247e8b6dc584f53717d0f9b3f221ca69ddf
sandymaravilhosa <44264267+sandymaravilhosa@users.noreply.github.com>
2020-05-03 23:37:47 -0300

    Add files via upload

---
+++ Pecas/Pecas/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Pecas
+{
+    class Pecas
+    {
+        static void Main(string[] args)
+        {
+            Pecas pecas = new Pecas();
+        }
+
+        private int idpecas;
+        private int idfuncionario;
+        private string nome;
+        private string designacao;
+        private double custounitario;
+        private double quantidade;
+
+
+        public Pecas() { }
+
+        public Pecas(int idpecas, int idfuncionario, string nome, string designacao, double custounitario, double quantidade)
+        {
+
+            this.idpecas = idpecas;
+            this.idfuncionario = idfuncionario;
+            this.nome = nome;
+            this.designacao = designacao;
+            this.custounitario = custounitario;
+            this.quantidade = quantidade;
+        }
+
+
+    }
+}
