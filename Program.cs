f9c2f656d50bffb42f352bc840f705344f6d1b79
sandymaravilhosa <44264267+sandymaravilhosa@users.noreply.github.com>
2020-05-03 23:28:58 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace CarroCliente
+{
+    class CarroCliente
+    {
+        static void Main(string[] args)
+        {
+            CarroCliente carrocliente = new CarroCliente();
+        }
+        private int idcarro;
+        private int idcliente;
+        private string modelo;
+        private string placa;
+        private DateAndTime data;
+        
+
+        public CarroCliente() { }
+
+        public CarroCliente(int idcarro, int idcliente, string modelo, string placa, DateAndTime data)
+        {
+
+            this.idcarro = idcarro;
+            this.idcliente = idcliente;
+            this.modelo = modelo;
+            this.placa = placa;
+            this.data = data;
+        }
+
+
+
+    }
+}
