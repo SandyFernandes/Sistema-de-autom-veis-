a0927a87adbb7efe4efe38e2bff6078e0fb0a393
sandymaravilhosa <44264267+sandymaravilhosa@users.noreply.github.com>
2020-05-03 23:34:58 -0300

    Add files via upload

---
+++ Cliente/Cliente/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Cliente
+{
+    class Cliente
+    {
+        static void Main(string[] args)
+        {
+            Cliente cliente = new Cliente();
+        }  
+        
+            private int idcliente;
+            private string nome;
+            private string morada;
+            private int telefone;
+            private int idcarro;
+
+
+        public Cliente() { }
+
+            public Cliente(int idcliente, string nome, string morada, int telefone, int idcarro)
+            {
+
+                this.idcliente= idcliente;
+                this.nome = nome;
+                this.morada = morada;
+                this.telefone = telefone;
+                this.idcarro = idcarro;
+
+            }
+     
+    }
+}
