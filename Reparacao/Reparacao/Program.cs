00cf7dbe2620a95995152cf2fc6c2509470fd4b9
sandymaravilhosa <44264267+sandymaravilhosa@users.noreply.github.com>
2020-05-03 23:39:18 -0300

    Add files via upload

---
+++ Reparacao/Reparacao/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Reparacao
+{
+    class Reparacao
+    {
+        static void Main(string[] args)
+        {
+            Reparacao reparacao = new Reparacao();
+        }
+        private int idcarro;
+        private int idpecas;
+        private int idfuncionario;
+        private int idcliente;
+        private double pecospecas;
+        private DateTime datareparacao;
+        private string pecasutilizadas;
+        private double custoreparacao;
+        private double pecosmaoobrafuncionario;
+        private double respectivocustos;
+
+
+        public Reparacao() { }
+
+        public Reparacao(int idcarro, int idpecas, int idfuncionario, int idcliente, double pecospecas, 
+        DateTime datareparacao,string pecasutilizadas, double custoreparacao,double pecosmaoobrafuncionario, double respectivocustos)
+        {
+
+            this.idcarro = idcarro;
+            this.idpecas = idpecas;
+            this.idfuncionario = idfuncionario;
+            this.idcliente = idcliente;
+            this.pecospecas = pecospecas;
+            this.datareparacao = datareparacao;
+            this.pecasutilizadas = pecasutilizadas;
+            this.custoreparacao = custoreparacao;
+            this.pecosmaoobrafuncionario = pecosmaoobrafuncionario;
+            this.respectivocustos = respectivocustos;
+        }
+    }
+}
