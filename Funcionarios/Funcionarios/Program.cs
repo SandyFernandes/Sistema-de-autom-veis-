63ee1b401691b0d89f83f85fcf5438430ef965cd
sandymaravilhosa <44264267+sandymaravilhosa@users.noreply.github.com>
2020-05-03 23:36:57 -0300

    Add files via upload

---
+++ Funcionarios/Funcionarios/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+
+namespace Funcionarios
+{
+    class Funcionarios
+    {
+        static void Main(string[] args)
+        {
+           Funcionarios funcionarios = new Funcionarios();
+        }
+
+        private int idfuncionario;
+        private string nome;
+        private string morada;
+        private string cargo;
+        private int telefone;
+        private DateTime dataadmissao;
+
+
+        public Funcionarios() { }
+
+        public Funcionarios(int idfuncionario, string nome, string morada, string cargo, int telefone, DateTime dataadmissao)
+        {
+
+            this.idfuncionario = idfuncionario;
+            this.nome = nome;
+            this.morada = morada;
+            this.cargo = cargo;
+            this.telefone = telefone;
+            this.dataadmissao = dataadmissao;
+            
+
+        }
+
+    }
+}
